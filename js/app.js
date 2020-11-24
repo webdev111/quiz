8023aa2a2c340090791505b1953b4d5feb02cb70
mayursaroj <mayursarojmusic@gmail.com>
2020-11-24 19:29:48 +0530

    first commit, starter project

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const App = (() => {
+  // cache the DOM
+  const quizEl = document.querySelector(".jabquiz");
+  const quizQuestionEl = document.querySelector(".jabquiz__question");
+  const trackerEl = document.querySelector(".jabquiz__tracker");
+  const taglineEl = document.querySelector(".jabquiz__tagline");
+  const choicesEl = document.querySelector(".jabquiz__choices");
+  const progressInnerEl = document.querySelector(".progress__inner");
+  const nextButtonEl = document.querySelector(".next");
+  const restartButtonEl = document.querySelector(".restart");
+
+
+})();
+
+
